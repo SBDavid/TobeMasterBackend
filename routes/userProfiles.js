c009e9bde379f70a079e3905d5fa8e4bdc5149c1
davidtangjw <davidtangjw@pptv.com>
2017-04-05 15:39:37 +0800

    用户查询

--- routes/userProfiles.js
+++ routes/userProfiles.js
@@ -5,14 +5,27 @@ var AV = require('leanengine');
 var responseUtils = require('./utils/response');
 var userProfileService = require('../service/UserProfile');
 
+// 查询用户
+router.get('/get/:account', function(req, res, next) {
+
+    userProfileService.get(req.params.account).then(
+        function(userProfile) {
+            res.send(responseUtils.success(userProfile));
+        },
+        function(err) {
+            res.send(responseUtils.fail(err));
+        })
+});
+
+// 添加用户
 router.post('/regist', function(req, res, next) {
 
-  userProfileService.save(req, res).then(
+  userProfileService.save(req.body.account, req.body.name).then(
   	function(userProfile) {
       res.send(responseUtils.success(userProfile));
     },
     function(err) {
-      res.send(responseUtils.fail(JSON.stringify(err)));
+      res.send(responseUtils.fail(err));
   	})
 });
 
--- service/UserProfile.js
+++ service/UserProfile.js
@@ -4,14 +4,32 @@ var UserProfile = AV.Object.extend('UserProfile');
 
 var userProfile = {
     // 获取用户
+    get: function(account) {
+        // account不能为空
+        if (account == undefined) {
+            return Promise.reject('account不能为空！');
+        }
 
+        var query = new AV.Query(UserProfile);
+
+        query.equalTo('account', account);
+
+        return query.find();
+    },
     // 保存用户
-    save: function (req, res) {
-        var name = req.body.name;
+    save: function (account, name) {
+        name = name || 'Nan';
+
+        // account不能为空
+        if (account == undefined) {
+            return Promise.reject('account不能为空！');
+        }
+
         var userProfile = new UserProfile();
 
         userProfile.set('name', name);
-        return userProfile.save(null, {sessionToken: req.sessionToken});
+        userProfile.set('account', account);
+        return userProfile.save();
     }
 };
 
